316eb313597c2581644eccd979431d42359ea63a
fedor <fedor.malyshkin@yandex.ru>
2020-05-17 22:31:39 +0400

    Add series of improvements

--- english/speak/storage/builder.py
+++ english/speak/storage/builder.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from . import storage
 
@@ -32,8 +33,28 @@ def flat_file(file_name):
     return storage.Storage(wrap_flat_records(read_lines(file_name)))
 
 
-def process_record(rec):
-    verb = rec.get("name")
+def flat_file_pairs(file_name, transformer):
+    t = read_lines(file_name)
+    data = []
+    for e in list(zip(t[::2], t[1::2])):
+        rec = {"name": e[0], "meaning": e[1]}
+        rec = transformer(rec)
+        data.append(rec)
+    return storage.Storage(data)
+
+
+def read_lines(file_name):
+    with open(file_name) as f:
+        value = f.read().splitlines()
+        return [rec for rec in value if not is_empty(rec)]
+
+
+def wrap_flat_records(records):
+    return [{"name": rec} for rec in records]
+
+
+def phrasal_verb_record_transformer(original_record):
+    verb = original_record.get("name")
     details = []
     if verb.find(INFORMAL) != -1:
         verb = verb.replace(INFORMAL, "")
@@ -60,25 +81,15 @@ def process_record(rec):
         verb = verb.replace(OFFENSIVE, "")
         details.append(OFFENSIVE)
 
-    rec.update({"name": verb.strip(), "note": ", ".join(details)})
-    return rec
+    reg_expression = r'\(([0-9]+)\)'
+    m = re.search(reg_expression, verb)
+    if m is not None:
+        verb = re.sub(reg_expression, "", verb)
+        details.append("var. " + m.group(1))
+
+    original_record.update({"name": verb.strip(), "note": ", ".join(details)})
+    return original_record
 
 
-def flat_file_pairs(file_name):
-    t = read_lines(file_name)
-    data = []
-    for e in list(zip(t[::2], t[1::2])):
-        rec = {"name": e[0], "meaning": e[1]}
-        rec = process_record(rec)
-        data.append(rec)
-    return storage.Storage(data)
-
-
-def read_lines(file_name):
-    with open(file_name) as f:
-        value = f.read().splitlines()
-        return [rec for rec in value if not is_empty(rec)]
-
-
-def wrap_flat_records(records):
-    return [{"name": rec} for rec in records]
+def dumb_transformer(original_record):
+    return original_record
